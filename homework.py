a966e8170309649d2b09a8630089a63fdd3e4655
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-11-09 15:43:28 +0300

    Правки

--- homework.py
+++ homework.py
@@ -24,7 +24,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = RotatingFileHandler('my_logger.log', maxBytes=50000000, backupCount=5)
+handler = RotatingFileHandler(
+    'my_logger.log', maxBytes=50000000, backupCount=5
+)
 formatter = logging.Formatter(
     '%(asctime)s - %(levelname)s - %(message)s'
 )
